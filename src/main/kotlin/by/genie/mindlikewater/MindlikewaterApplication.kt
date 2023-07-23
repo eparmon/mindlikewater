0e2ea8cb5bf8c68292d12ea21b5f88351592c5c8
Gene <eparmon@tuta.io>
2023-07-23 14:15:38 +0400

    Initialize a Spring Boot app

---
+++ src/main/kotlin/by/genie/mindlikewater/MindlikewaterApplication.kt
@@ -0,0 +1,11 @@
+package by.genie.mindlikewater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MindlikewaterApplication
+
+fun main(args: Array<String>) {
+	runApplication<MindlikewaterApplication>(*args)
+}
---
+++ src/test/kotlin/by/genie/mindlikewater/MindlikewaterApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.genie.mindlikewater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MindlikewaterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
